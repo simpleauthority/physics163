06208cd3ebad4e12534555a3e56f043e2447bda9
simpleauthority <jacob@algorithmjunkie.com>
2021-11-03 02:43:26 -0700

    whatever i can't figure out the loss cone so. "it exists" is good enough

--- src/chapter/28/MagneticBottleAllPoi.py
+++ src/chapter/28/MagneticBottleAllPoi.py
@@ -1,28 +1,32 @@
 from vpython import *
 
 # design
-scene.title = "<h1>Magnetic Bottle Simulation</h1><br /><p>Watch as the particle gets stuck</p><br /><br />"
-scene.width = 1024
-scene.height = 768
+scene.title = "<h1>Magnetic Bottle Simulation</h1><p>Electron initially stuck in a magnetic bottle. The particle will corkscrew within the bottle until it his a loss cone (not depicted) at which point<br />it will leave the system. Simulation does not stop on its own. Press \"Stop simulation\" or close the page when done.<br />"
+scene.width = 960
+scene.height = 720
 scene.background = vec(161, 152, 151) / 255
 
-
 # constants
-ring_radius = 0.315  # radius of ring, meters
+ring_radius = 0.105  # radius of ring, meters
 num_slices = 360  # how many slices to cut ring into
 theta_min = 0  # min angle of ring, radians
 theta_max = 2 * pi  # max angle of ring, radians
 total_theta = theta_max - theta_min  # total angle of ring, radians
 dtheta = total_theta / num_slices  # differential angle of each slice, radians
 mu_naught = (4 * pi) * 1e-7  # tesla*meter/sec
-current = -500  # amps
+current = -500 # amps
 integration_constant = (mu_naught * current) / (4 * pi)  # constant out front of BS law integral
 
 # size constants
-x_min = z_min = -3
-y_min = -0.75
-x_max = z_max = 5
-y_max = 2
+x_min = -2
+z_min = -1
+y_min = -0.5
+x_max = 4
+z_max = 2
+y_max = 1
+
+# sim settings
+run_sim = False
 
 # axes
 line_x = cylinder(pos=vec(x_min, 0, 0), axis=vec(x_max, 0, 0), radius=0.0025, color=color.black)
@@ -36,120 +40,205 @@ label_z = label(pos=line_z.pos + line_z.axis, text='z', height=16, color=color.b
 
 # set up camera
 scene.camera.rotate(radians(-15), vec(1, 0, 0))
-scene.camera.rotate(radians(15), vec(0, 1, 0))
-scene.camera.pos += vec(0.45, 0.30, -0.6)
+scene.camera.rotate(radians(18), vec(0, 1, 0))
+scene.camera.pos += vec(0.6, 0.3, -1.25)
 scene.autoscale = False
 
 
-# get direction of any theta position on the ring
-def calc_theta_hat(theta):
-    return vec(0, cos(theta), -sin(theta))  # yz plane, x set to zero
-
-
-# get differential arclength of ring
-def calc_ds(theta):
-    return ring_radius * dtheta * calc_theta_hat(theta)
-
-
-# calculate the magnetic field of the ring at a specific position
-def calc_mag_field(ring, poi):
-    B_total = vec(0, 0, 0)  # total magnetic field
+# essential methods
+# calculate the magnetic field caused by the given rings at a given point of interest
+def calc_mag_field(rings, point_of_interest):
+    b_total = vec(0, 0, 0)  # total magnetic field
 
     for theta in arange(theta_min, theta_max, dtheta):
-        segment = vec(ring.pos.x, ring.pos.y + ring_radius * sin(theta), ring.pos.z + ring_radius * cos(theta))
-        r = poi - segment  # r vector from segment to arbitrary POI --ring in yz plane--
-        r_mag = mag(r)  # mag of r vector
+        theta_hat = vec(0, cos(theta), -sin(theta))
+        ds = ring_radius * dtheta * theta_hat
 
-        ds = calc_ds(theta)  # differential arclength of a specific theta
+        for ring in rings:
+            segment = vec(ring.pos.x, ring.pos.y + ring_radius * sin(theta), ring.pos.z + ring_radius * cos(theta))
+            r = point_of_interest - segment  # r vector from segment to arbitrary POI --ring in yz plane--
+            r_mag = mag(r)  # mag of r vector
 
-        if r_mag != 0:
-            B_total += integration_constant * ((cross(ds, r)) / r_mag ** 3)  # append differential B to total B
+            if r_mag != 0:
+                b_total += integration_constant * ((cross(ds, r)) / r_mag ** 3)  # append differential B to total B
 
-    return B_total
+    return b_total
 
 
-def scale_factor(b):
-    factor = ring_radius / mag(b)
+# calculate a scale factor proportional to the ring_radius for a given vector
+def scale_factor(b, prop_to=ring_radius):
+    factor = prop_to / mag(b)
     # print("Scale factor will be ", factor)
     return factor
 
 
-# draw left ring
-left_ring = ring(pos=vec(x_min, 0, 0), axis=vec(1, 0, 0), radius=ring_radius, thickness=0.01, color=color.purple)
+# draw a ring
+def create_ring(pos):
+    return ring(pos=pos, axis=vec(1, 0, 0), radius=ring_radius, thickness=0.01, color=color.purple)
 
-# draw right ring
-right_ring = ring(pos=vec(x_min+x_max, 0, 0), axis=vec(1, 0, 0), radius=ring_radius, thickness=0.01, color=color.purple)
 
-print(f"Magnetic field at origin: {calc_mag_field(left_ring, vec(0, 0, 0)) + calc_mag_field(right_ring, vec(0, 0, 0))}")
+# draw rings
+left_ring = create_ring(vec(x_min, 0, 0))
+right_ring = create_ring(vec(x_min+x_max, 0, 0))
 
-# draw pois and calc data for every point in space
-dx = dy = dz = 0.35
-B_min = B_max = 0
-for x in arange(x_min + dx, (x_max / 2) - dx/2, dx):
-    for y in arange(y_min / 3, y_max / 3, dy):
-        for z in arange((z_min / 3) - dz, (z_max / 3), dz):
-            poi = vec(x, y, z)
-            exp_b = calc_mag_field(left_ring, poi) + calc_mag_field(right_ring, poi)
+# proton settings
+electron_init = dict()
+electron_init['pos'] = vec(0, ring_radius / 8, 0)
+electron_init['charge'] = -1.602e-19
+electron_init['mass'] = 9.10938356e-31
+electron_init['b_field'] = calc_mag_field([left_ring, right_ring], electron_init['pos'])
+electron_init['velocity'] = vec(0, 0, ((ring_radius * electron_init['charge'] * mag(electron_init['b_field'])) / (8 * electron_init['mass'])))
+electron_init['velocity'].x = electron_init['velocity'].z
+electron_init['momentum'] = electron_init['mass'] * electron_init['velocity']
+electron_init['b_force'] = cross(electron_init['charge'] * electron_init['velocity'], electron_init['b_field'])
 
-            b_arrow = arrow(pos=poi, axis=exp_b * scale_factor(exp_b), color=color.blue)
 
-            exp_b_mag = mag(exp_b)
-            if exp_b_mag > B_max:
-                B_max = exp_b_mag
-            elif exp_b_mag < B_min:
-                B_min = exp_b_mag
+# draw the b field over points in space
+def draw_b_field():
+    arrow_sat = 0.15
 
-            if exp_b_mag > 0:
-                b_arrow.opacity = exp_b_mag / B_max
-            elif exp_b_mag < 0:
-                b_arrow.opacity = exp_b_mag / B_min
+    dx = dy = dz = 0.5
 
-            b_arrow.opacity *= 0.7
+    x_start = x_min
+    y_start = y_min
+    z_start = z_min
 
-# draw an electron in the b field
-proton = sphere(pos=vec(0, ring_radius / 15, 0), radius=0.05, color=color.red)  # electron
-proton.charge = 1.602e-19  # electron elementary charge
-proton.mass = 1.672e-27  # mass of electron
-proton.magnetic_field = calc_mag_field(left_ring, proton.pos) + calc_mag_field(right_ring, proton.pos)
-proton.velocity = vec(0, 0, -((ring_radius * proton.charge * mag(proton.magnetic_field)) / (15 * proton.mass)))   # initial velocity of proton, moving in k-hat
-proton.velocity.x=7*proton.velocity.z
-# proton.velocity_arrow = arrow(pos=proton.pos, axis=proton.velocity * scale_factor(proton.velocity), color=color.purple)
-proton.momentum = proton.mass * proton.velocity
-proton.magnetic_force = cross(proton.charge * proton.velocity, proton.magnetic_field)
-# proton.force_arrow = arrow(pos=proton.pos, axis=proton.magnetic_force * scale_factor(proton.magnetic_force), color=color.orange)
+    x_end = x_max + x_min
+    y_end = y_max + y_min
+    z_end = z_max + z_min
 
-# animate the electron
-t = 0
-dt = (2 * pi * proton.pos.y) / (100 * proton.charge * mag(proton.velocity))
+    for x in arange(x_min, x_max, dx):
+        for y in arange(y_min, y_max, dy):
+            for z in arange(z_min, z_max, dz):
+                poi = vec(x, y, z)
+                b_at_poi = calc_mag_field([left_ring, right_ring], poi)
 
-while True:
-    # rate loop for testing
-    rate(60)
+                if x_start <= x <= x_end and y_start <= y <= y_end and z_start <= z <= z_end:
+                    b_arrow = arrow(pos=poi, axis=b_at_poi * 1e6, color=color.blue, opacity=0.15)
 
-    b_field = calc_mag_field(left_ring, proton.pos) + calc_mag_field(right_ring, proton.pos)
-    print(f"Magnetic field is {b_field}T ({mag(b_field)}T)")
+                    if mag(b_arrow.axis) > arrow_sat:
+                        b_arrow.axis = arrow_sat * hat(b_arrow.axis)
 
-    # find force on electron due to B_total
-    proton.magnetic_force = cross(proton.charge * proton.velocity, b_field)
-    print(f"Force on electron is {proton.magnetic_force}N ({mag(proton.magnetic_force)}N)")
+                    b_arrow.b = b_at_poi
+                    b_arrow.poi = poi
 
-    # update electron's momentum
-    proton.momentum += proton.magnetic_force * dt
-    print(f"Momentum of electron is {proton.momentum}kgm/s")
 
-    # update electron's velocity
-    proton.velocity = proton.momentum / proton.mass
-    print(f"Velocity of electron is {proton.velocity}m/s")
+# create physical proton from given settings
+def set_electron(settings, already_exists=None):
+    exists = already_exists is not None
 
-    # update the position due to the updated momentum
-    proton.pos += (proton.momentum / proton.mass) * dt
-    print(f"Position of electron is ({proton.pos.x:.2e}, {proton.pos.y:.2e}, {proton.pos.z:.2e})")
+    out = already_exists if exists else sphere(radius=0.025, color=color.red, make_trail=True, retain=150)
+    out.pos = settings['pos']
+    out.charge = settings['charge']
+    out.mass = settings['mass']
+    out.b_field = settings['b_field']
+    out.velocity = settings['velocity']
+    out.vperp2b = cross(out.velocity, out.b_field)
+    out.momentum = settings['momentum']
+    out.b_force = settings['b_force']
 
-    # draw arrows
-    # proton.velocity_arrow.pos = proton.pos
-    # proton.velocity_arrow.axis = proton.velocity * 1e-3
-    # proton.force_arrow.pos = proton.pos
-    # proton.force_arrow.axis = proton.magnetic_force * 1e21
+    label_pos = out.pos + vec(0, 0.25, 0)
+    label_text = f"{mag(out.velocity):.4f}m/s"
 
-    print()
-    t += dt
+    if exists:
+        out.b_field_arrow.pos = out.pos
+        out.b_field_arrow.axis = out.b_field * scale_factor(out.b_field, prop_to=out.radius)
+        out.velocity_label.pos = label_pos
+        out.velocity_label.text = label_text
+        out.velocity_arrow.pos = out.pos
+        out.velocity_arrow.axis = out.velocity * scale_factor(out.velocity, prop_to=out.radius)
+        out.force_arrow.pos = out.pos
+        out.force_arrow.axis = out.b_force * scale_factor(out.b_force, prop_to=out.radius)
+    else:
+        out.b_field_arrow = arrow(pos=out.pos, axis=out.b_field * scale_factor(out.b_field), color=color.magenta)
+        out.velocity_label = label(pos=label_pos, text=label_text, height=10)
+        out.velocity_arrow = arrow(pos=out.pos, axis=out.velocity * scale_factor(out.velocity), color=color.purple)
+        out.force_arrow = arrow(pos=out.pos, axis=out.b_force * scale_factor(out.b_force), color=color.orange)
+
+    return out
+
+
+# draw proton
+electron = set_electron(electron_init)
+
+# draw b field
+draw_b_field()
+# print(f"Minimum B seen: {b_min}")
+# print(f"Maximum B seen: {b_max}")
+
+
+def reset_electron():
+    global run_sim, electron_init, electron
+
+    run_sim = False
+    electron = set_electron(electron_init, electron)
+    electron.clear_trail()
+
+
+def simulate_proton():
+    global run_sim, electron
+
+    t = 0
+    dt = 12e-8
+
+    while True:
+        # rate loop to 60 itr/sec
+        rate(120)
+
+        # if the sim should not run, skip iteration
+        if not run_sim:
+            continue
+
+        b_field = calc_mag_field([left_ring, right_ring], electron.pos)
+        # print(f"Magnetic field is {b_field}T ({mag(b_field)}T)")
+
+        # find force on electron due to B_total
+        electron.magnetic_force = cross(electron.charge * electron.velocity, b_field)
+        # print(f"Force on electron is {proton.magnetic_force}N ({mag(proton.magnetic_force)}N)")
+
+        # update electron's momentum
+        electron.momentum += electron.magnetic_force * dt
+        # print(f"Momentum of electron is {proton.momentum}kgm/s")
+
+        # update electron's velocity
+        electron.velocity = electron.momentum / electron.mass
+        electron.vperp2b = cross(electron.velocity, b_field)
+        # print(f"Velocity of electron is {proton.velocity}m/s")
+
+        # update the position due to the updated momentum
+        electron.pos += electron.velocity * dt
+        # print(f"Position of electron is ({proton.pos.x:.2e}, {proton.pos.y:.2e}, {proton.pos.z:.2e})")
+
+        # update velocity label
+        electron.velocity_label.text = f"{mag(electron.velocity):.4f}m/s"
+        electron.velocity_label.pos = electron.pos + vec(0, 0.25, 0)
+
+        # update arrows
+        electron.b_field_arrow.pos = electron.pos
+        electron.b_field_arrow.axis = b_field * scale_factor(b_field)
+        electron.velocity_arrow.pos = electron.pos
+        electron.velocity_arrow.axis = electron.velocity * scale_factor(electron.velocity)
+        electron.force_arrow.pos = electron.pos
+        electron.force_arrow.axis = electron.magnetic_force * scale_factor(electron.magnetic_force)
+
+        # print()
+        t += dt
+
+
+def toggle_sim_running(b):
+    global run_sim
+    run_sim = not run_sim
+    if run_sim:
+        b.text = "Stop simulation"
+    else:
+        b.text = "Start simulation"
+
+
+# GUI stuff
+toggle_button = button(pos=scene.title_anchor, bind=toggle_sim_running, text="Start/stop simulation")
+scene.append_to_title("  ")
+reset_button = button(pos=scene.title_anchor, bind=reset_electron, text="Reset simulation")
+scene.append_to_title("\n\n")
+
+# start proton simulation
+simulate_proton()
